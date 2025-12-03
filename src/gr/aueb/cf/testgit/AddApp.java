d62be0c6fde7ac461b6b5222bb40fcd5d08b8c79
Vas. Neratzis <vas.neratzis@gmail.com>
2025-12-03 18:01:56 +0200

    featÎ:addApp

---
+++ src/gr/aueb/cf/testgit/AddApp.java
@@ -0,0 +1,10 @@
+package gr.aueb.cf.testgit;
+
+public class AddApp {
+    public static void main(String[] args) {
+        System.out.println(add(3,7));
+    }
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
